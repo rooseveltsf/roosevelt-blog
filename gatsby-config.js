ab939eb613ff52f03153d68bd1fd920f9b79a206
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-07-04 22:01:07 -0300

    Limpa estrutura do projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `RooseveltSouza`,
+    description: `O portfolio de um desenvolvedor full-stack`,
+    author: `@roosevellt`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
